d3ab6ec9ca892a6f63eacc7f75aec315eedbadd1
Sfeldman76 <scott.feldman@outlook.com>
2025-06-09 14:29:08 -0400

    Update sharp_line_dashboard.py

--- sharp_line_dashboard.py
+++ sharp_line_dashboard.py
@@ -1634,7 +1634,8 @@ def render_sharp_signal_analysis_tab(tab, sport_label, sport_key_api, drive):
 
         # ✅ Load master with Game_Key already handled
         df_master = load_master_sharp_moves(drive, folder_id=FOLDER_ID)
-        df_master = df_master[df_master['Sport'] == label.upper()]
+        df_master = df_master[df_master['Sport'] == sport_label.upper()]
+
 
 
         if df_master.empty:
