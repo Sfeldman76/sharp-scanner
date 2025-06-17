4ced49c682514c87dd319723e8bc1d611904ccbe
Sfeldman76 <scott.feldman@outlook.com>
2025-06-16 20:47:15 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+# config.py
+
+SPORTS = {
+    "NBA": "basketball_nba",
+    "MLB": "baseball_mlb",
+    "WNBA": "basketball_wnba",
+    "CFL": "americanfootball_cfl"
+}
+
+SHARP_BOOKS = [
+    "pinnacle", "bookmaker", "betonline", "heritage"
+]
+
+REC_BOOKS = [
+    "draftkings", "fanduel", "pointsbet", "betrivers", "mgm"
+]
+
+BOOKMAKER_REGIONS = {
+    "pinnacle": "offshore",
+    "bookmaker": "offshore",
+    "betonline": "offshore",
+    "heritage": "offshore",
+    "draftkings": "us",
+    "fanduel": "us",
+    "pointsbet": "us",
+    "betrivers": "us",
+    "mgm": "us"
+}
