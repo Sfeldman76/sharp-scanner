45322f77aeb0a4ee4f5288399badc979b3a7bb32
Sfeldman76 <scott.feldman@outlook.com>
2025-07-06 14:08:06 -0400

    Update sharp_line_dashboard.py

--- sharp_line_dashboard.py
+++ sharp_line_dashboard.py
@@ -547,12 +547,12 @@ def train_sharp_model_from_bq(sport: str = "NBA", days_back: int = 30):
             'Market_Leader',
             'LimitUp_NoMove_Flag',
             'Is_Sharp_Book',
-            'Line_Value_Abs',
-            #'Line_Delta',
-            'Direction_Aligned',
+            #'Line_Value_Abs',
+            'Line_Delta',
+            #'Direction_Aligned',
             #'Line_Move_Magnitude',
             'Is_Home_Team_Bet',
-            'High_Limit_Flag'
+            #'High_Limit_Flag'
         ]
         
        
