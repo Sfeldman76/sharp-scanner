b60522ed9c4f88afd2e34f0df4b498995832bb7e
Sfeldman76 <scott.feldman@outlook.com>
2025-07-08 16:13:16 -0400

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SPORTS = {
 }
 
 SHARP_BOOKS_FOR_LIMITS = ['pinnacle']
-SHARP_BOOKS = SHARP_BOOKS_FOR_LIMITS + ['lowvig']
+SHARP_BOOKS = SHARP_BOOKS_FOR_LIMITS
 
 REC_BOOKS = [
     'betmgm', 'bet365', 'draftkings', 'fanduel', 'betrivers',
