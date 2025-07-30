0080aa5e1c2c8c41145d002e7818a4484b1e048a
Sfeldman76 <scott.feldman@outlook.com>
2025-07-30 19:06:55 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -50,7 +50,7 @@ SPORTS = {
 
 
 SHARP_BOOKS_FOR_LIMITS = ['pinnacle']
-SHARP_BOOKS = SHARP_BOOKS_FOR_LIMITS + ['betus','mybookieag','betfair_ex_eu',betfair_sb_uk,'betfair_ex_uk','lowvig','betonlineag','matchbook']
+SHARP_BOOKS = SHARP_BOOKS_FOR_LIMITS + ['betus','mybookieag','betfair_ex_eu','betfair_sb_uk','betfair_ex_uk','lowvig','betonlineag','matchbook']
 
 REC_BOOKS = [
     'betmgm', 'bet365', 'draftkings', 'fanduel', 'betrivers',
