c8a0d2f6007b5a59fb6ae0851fbbfb1414fc395b
Sfeldman76 <scott.feldman@outlook.com>
2025-07-29 20:36:02 -0400

    Update sharp_line_dashboard.py

--- sharp_line_dashboard.py
+++ sharp_line_dashboard.py
@@ -901,7 +901,7 @@ def train_sharp_model_from_bq(sport: str = "NBA", days_back: int = 35):
         df_market['Market_Norm'] = df_market['Market'].str.lower()
         
         # === Absolute Line and Odds Movement
-        df_market['Abs_Line_Move_From_Opening'] = (df_market['Final_Line_Value'] - df_market['First_Line_Value']).abs()
+        df_market['Abs_Line_Move_From_Opening'] = (df_market['Value'] - df_market['First_Line_Value']).abs()
         df_market['Odds_Shift'] = df_market['Odds_Price'] - df_market['First_Odds']
         df_market['Implied_Prob_Shift'] = (
             calc_implied_prob(df_market['Odds_Price']) - calc_implied_prob(df_market['First_Odds'])
