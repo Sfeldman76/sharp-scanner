e53de2337039fcb590feb070c284a4df6a121e10
Sfeldman76 <scott.feldman@outlook.com>
2025-08-14 14:47:54 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -147,31 +147,31 @@ def update_power_ratings(
       - Append to ratings_history
       - Upsert ratings_current
 
-    Returns a small dict summary.
-    """
-    # ---------------- CONFIG (baseball / football / basketball only) ----------------
-SPORT_ALIASES = {
-    "MLB":   ["MLB", "BASEBALL_MLB", "BASEBALL-MLB", "BASEBALL"],
-    "NFL":   ["NFL", "AMERICANFOOTBALL_NFL", "FOOTBALL_NFL"],
-    "NCAAF": ["NCAAF", "AMERICANFOOTBALL_NCAAF", "CFB"],
-    "NBA":   ["NBA", "BASKETBALL_NBA"],
-    "WNBA":  ["WNBA", "BASKETBALL_WNBA"],
-    "CFL":   ["CFL", "CANADIANFOOTBALL", "CANADIAN_FOOTBALL"],
-}
-
-def get_aliases(canon: str) -> list[str]:
-    return SPORT_ALIASES.get(canon.upper(), [canon.upper()])
-
-    SPORT_CFG = {
-        "MLB":    dict(model="poisson", K_start=None, K_late=None, HFA=18.0, mov_cap=None),
-        "NFL":    dict(model="elo", K_start=22.0, K_late=12.0, HFA=22.0, mov_cap=24.0),
-        "NCAAF":  dict(model="elo", K_start=22.0, K_late=12.0, HFA=22.0, mov_cap=24.0),          
-        "NBA":    dict(model="elo", K_start=18.0, K_late=10.0, HFA=55.0, mov_cap=25.0),
-        "WNBA":   dict(model="elo", K_start=18.0, K_late=10.0, HFA=45.0, mov_cap=25.0),
-        "CFL":    dict(model="elo", K_start=20.0, K_late=12.0, HFA=18.0, mov_cap=30.0),
-        
-    }
+Returns a small dict summary.
+"""
+# ---------------- CONFIG (baseball / football / basketball only) ----------------
+   SPORT_ALIASES = {
+       "MLB":   ["MLB", "BASEBALL_MLB", "BASEBALL-MLB", "BASEBALL"],
+       "NFL":   ["NFL", "AMERICANFOOTBALL_NFL", "FOOTBALL_NFL"],
+       "NCAAF": ["NCAAF", "AMERICANFOOTBALL_NCAAF", "CFB"],
+       "NBA":   ["NBA", "BASKETBALL_NBA"],
+       "WNBA":  ["WNBA", "BASKETBALL_WNBA"],
+       "CFL":   ["CFL", "CANADIANFOOTBALL", "CANADIAN_FOOTBALL"],
+   }
    
+   def get_aliases(canon: str) -> list[str]:
+       return SPORT_ALIASES.get(canon.upper(), [canon.upper()])
+   
+       SPORT_CFG = {
+           "MLB":    dict(model="poisson", K_start=None, K_late=None, HFA=18.0, mov_cap=None),
+           "NFL":    dict(model="elo", K_start=22.0, K_late=12.0, HFA=22.0, mov_cap=24.0),
+           "NCAAF":  dict(model="elo", K_start=22.0, K_late=12.0, HFA=22.0, mov_cap=24.0),          
+           "NBA":    dict(model="elo", K_start=18.0, K_late=10.0, HFA=55.0, mov_cap=25.0),
+           "WNBA":   dict(model="elo", K_start=18.0, K_late=10.0, HFA=45.0, mov_cap=25.0),
+           "CFL":    dict(model="elo", K_start=20.0, K_late=12.0, HFA=18.0, mov_cap=30.0),
+           
+       }
+      
 
     def elo_expected(delta, hfa=20.0):
         return 1.0 / (1.0 + 10.0 ** (-(delta + hfa) / 400.0))
