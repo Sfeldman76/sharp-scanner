63d67919d44e79bae0eea859364ccfda5c1a066c
Sfeldman76 <scott.feldman@outlook.com>
2025-08-31 17:24:13 +0000

    Update sharp_line_dashboard.py

--- sharp_line_dashboard.py
+++ sharp_line_dashboard.py
@@ -4579,7 +4579,7 @@ def train_sharp_model_from_bq(sport: str = "NBA", days_back: int = 35):
             'Book_Reliability_x_PROB_SHIFT',
         
             # Power ratings / edges
-            #'PR_Team_Rating',#'PR_Opp_Rating',
+            'PR_Team_Rating','PR_Opp_Rating',
             'PR_Rating_Diff',#'PR_Abs_Rating_Diff',
             #'Outcome_Model_Spread','Outcome_Market_Spread',
             'Outcome_Spread_Edge',
