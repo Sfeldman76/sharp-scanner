89ab0545ed88f1b011bc472b299f58d56c2123af
Sfeldman76 <scott.feldman@outlook.com>
2026-02-17 11:46:47 -0500

    Refactor training functions for sharp model

--- train_sharp_model_from_bq_extracted.py
+++ train_sharp_model_from_bq_extracted.py
@@ -1,7 +1,15 @@
-# train_sharp_model_from_bq_extracted.py
-from training_entrypoints import (
-    train_sharp_model_for_market,
-    train_timing_model_for_market,
+from sharp_line_dashboard import (
+    train_with_champion_wrapper,
+    train_timing_opportunity_model,
 )
 
-__all__ = ["train_sharp_model_for_market", "train_timing_model_for_market"]
+def train_sharp_model_for_market(*, sport: str, market: str, bucket_name: str, log_func=print, **kwargs):
+    return train_with_champion_wrapper(
+        sport=sport,
+        market=market,
+        bucket_name=bucket_name,
+        **kwargs,
+    )
+
+def train_timing_model_for_market(*, sport: str, bucket_name: str = None, log_func=print, **kwargs):
+    return train_timing_opportunity_model(sport=sport)
