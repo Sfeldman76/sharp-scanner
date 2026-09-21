64c4b0fb9b9d99c780b926fca8570aa1c910bfe8
Sfeldman76 <scott.feldman@outlook.com>
2026-09-21 18:59:00 -0400

    Update V1337_WRAPPER_SOURCE_TAG version

--- train_sharp_model_from_bq_extracted.py
+++ train_sharp_model_from_bq_extracted.py
@@ -1,4 +1,4 @@
-V1337_WRAPPER_SOURCE_TAG = "wrapper-v13.3.8-core-oof-side-role-bridge"
+V1337_WRAPPER_SOURCE_TAG = "wrapper-v13.3.9-outer-stage-ablation"
 
 from sharp_line_dashboard import (
     train_with_champion_wrapper,
