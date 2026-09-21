e4341ddaee1932d3fc37a86bed1610494368e7d4
Sfeldman76 <scott.feldman@outlook.com>
2026-09-21 13:31:11 -0400

    Add V1337_WRAPPER_SOURCE_TAG constant

--- train_sharp_model_from_bq_extracted.py
+++ train_sharp_model_from_bq_extracted.py
@@ -1,3 +1,5 @@
+V1337_WRAPPER_SOURCE_TAG = "wrapper-v13.3.7-oof-bridge-calibration-hardening"
+
 from sharp_line_dashboard import (
     train_with_champion_wrapper,
     train_timing_opportunity_model,
