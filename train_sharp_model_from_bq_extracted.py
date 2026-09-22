f526342a0d2c51a73e3e7b2bcbf521112dc76a90
Sfeldman76 <scott.feldman@outlook.com>
2026-09-22 15:24:26 -0400

    Update train_sharp_model_from_bq_extracted.py

--- train_sharp_model_from_bq_extracted.py
+++ train_sharp_model_from_bq_extracted.py
@@ -1,4 +1,4 @@
-V1337_WRAPPER_SOURCE_TAG = "wrapper-v13.3.11.3-conditional-incremental-admission"
+V1337_WRAPPER_SOURCE_TAG = "wrapper-v13.4.0-production-resolver-freeze"
 
 from sharp_line_dashboard import (
     train_with_champion_wrapper,
