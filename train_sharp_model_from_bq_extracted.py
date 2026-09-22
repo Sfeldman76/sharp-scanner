e0d9c8850773c16bd6c2fe09cdd52b17974fdb26
Sfeldman76 <scott.feldman@outlook.com>
2026-09-22 09:54:01 -0400

    Update wrapper source tag for model training

--- train_sharp_model_from_bq_extracted.py
+++ train_sharp_model_from_bq_extracted.py
@@ -1,4 +1,4 @@
-V1337_WRAPPER_SOURCE_TAG = "wrapper-v13.3.11-expanded-chrono-validation"
+V1337_WRAPPER_SOURCE_TAG = "wrapper-v13.3.11.1-all-brain-chrono-validation"
 
 from sharp_line_dashboard import (
     train_with_champion_wrapper,
