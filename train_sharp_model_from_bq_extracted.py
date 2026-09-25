7e0254212ae4b06753bac6120c7bde274bf7d286
Sfeldman76 <scott.feldman@outlook.com>
2026-09-25 15:46:08 -0400

    Update train_sharp_model_from_bq_extracted.py

--- train_sharp_model_from_bq_extracted.py
+++ train_sharp_model_from_bq_extracted.py
@@ -1,4 +1,4 @@
-V1337_WRAPPER_SOURCE_TAG = "wrapper-v13.5.0.1-three-market-shadow-ui-maskfix"
+V1337_WRAPPER_SOURCE_TAG = "wrapper-v13.5.1-research-challengers"
 
 from sharp_line_dashboard import (
     train_with_champion_wrapper,
